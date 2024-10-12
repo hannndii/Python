197d9f8f7a526a52dd730d289e4766390c8468d8
hannndii <shiirogami95@gmail.com>
2024-10-12 11:11:10 +0700

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+data = int(input("Pilih:"))
+if data == 1:
+    print("OPERASI PERTAMBAHAN!")
+    data1 = int(input("Masukkan x: "))
+    data2 = int(input("Masukkan y: "))
+    print("Jumlah", data1, "+", data2, "= ", data1 + data2)
+
+else :
+    print("Silahkan pilih (1-5)!")
+
